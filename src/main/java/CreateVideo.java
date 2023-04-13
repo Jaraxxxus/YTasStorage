1f20315a971a3e5aef597bbe1753de7dae5bfe98
Sazonov Vyacheslav <Sazonov@nsk.softlab.ru>
2023-04-13 17:17:16 +0700

    early work

--- src/main/java/CreateVideo.java
+++ src/main/java/CreateVideo.java
@@ -6,23 +6,25 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Random;
 
 import static com.xuggle.xuggler.Global.DEFAULT_TIME_UNIT;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class CreateVideo {
     public static void main(String[] args) {
-        // the clock time of the next frame
-
+        // time of the next frame
         long nextFrameTime = 0;
+        ImageGenerator imageGenerator = new ImageGenerator(1280, 720, 16);
 
         // video parameters
 
         final int videoStreamIndex = 0;
         final int videoStreamId = 0;
         final long frameRate = DEFAULT_TIME_UNIT.convert(100, MILLISECONDS);
-        final int width = 320;
-        final int height = 200;
+        final int width = 1280;
+        final int height = 720;
 
         // audio parameters
 
@@ -34,7 +36,7 @@ public class CreateVideo {
 
         try {
 
-            final IMediaWriter writer = ToolFactory.makeWriter("test.mov");
+            final IMediaWriter writer = ToolFactory.makeWriter("res.mov");
 
             writer.addListener(ToolFactory.makeViewer(
                     IMediaViewer.Mode.VIDEO_ONLY, true,
@@ -50,13 +52,17 @@ public class CreateVideo {
             BufferedImage curFrame = frame;
             int x = 0;
             int y = 0;
-
+            Random random = new Random();
             for (int i = 0; i < 100; i++){
-                g2d.fillRect(0 + x, 0 + y, 16 + x, 16 + y);
-                writer.encodeVideo(videoStreamIndex, curFrame, nextFrameTime, DEFAULT_TIME_UNIT);
+                ArrayList<Integer> nums = new ArrayList<Integer>();
+                for (int j= 0 ; j < 57600 ; j++){
+                    nums.add(random.nextInt(25));
+                }
+
+
+                writer.encodeVideo(videoStreamIndex, imageGenerator.generateImage(nums), nextFrameTime, DEFAULT_TIME_UNIT);
                 nextFrameTime += frameRate;
-                x = (int)(Math.random()*300);
-                y = (int)(Math.random()*300);
+
 
 
             }
---
+++ src/main/java/ImageGenerator.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static com.xuggle.xuggler.Global.DEFAULT_TIME_UNIT;
+
+public class ImageGenerator {
+    int width, height;
+    int rectSize = 16;
+    int capacity;
+    HashMap<Integer, Color> colorsCode = new HashMap<>();
+    {
+        int key = 0;
+        for (int i = 0; i <= 255; i = i += 15) {
+            for (int j = 0; j <= 255; j = j += 15) {
+                for (int k = 0; k <= 255; k = k += 15) {
+                    colorsCode.put(key, new Color(i, j, k));
+                    ++key;
+                }
+            }
+        }
+
+    }
+
+    public ImageGenerator(int width, int height, int rectSize) {
+        this.width = width;
+        this.height = height;
+        this.rectSize = rectSize;
+        this.capacity = (width * height) / rectSize;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public  BufferedImage generateImage(ArrayList<Integer> keys){
+
+        BufferedImage frame = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        Graphics2D g2d = frame.createGraphics();
+
+        BufferedImage curFrame = frame;
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < capacity; i++){
+            g2d.setColor(colorsCode.get(keys.get(i)));
+            g2d.fillRect(rectSize * i, rectSize * i, rectSize, rectSize);
+        }
+        return frame;
+    }
+}
